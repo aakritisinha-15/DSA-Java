180f4edf433a8731793f2dae21cef0b0e1133d18
aakritisinha-15 <Myselfaakriti4@gmail.com>
2026-01-12 21:16:39 +0530

    Create ArrayIntro.java

---
+++ Array/ArrayIntro.java
@@ -0,0 +1,20 @@
+
+public class ArrayINTRO {
+
+	public static void main(String[] args) {
+		// vedio(91)-introduction to array(pepcodding)
+		//we have to store marks of multiple students
+		int [] arr=new int[5];
+		
+		arr[0]=30;
+		arr[1]=31;
+		arr[2]=32;
+		arr[3]=33;
+		arr[4]=34;
+		
+		for(int i=0; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
